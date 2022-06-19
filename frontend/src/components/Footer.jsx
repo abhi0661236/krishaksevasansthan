f7e6b977d31abde9966e7edd757f7a94b5bbca70
abhi0661236 <ap0661236@gmail.com>
2022-06-19 16:29:14 +0530

    improved the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,42 +1,54 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+
 import '../assets/css/Footer.css';
 import Logo from '../assets/media/logo192.png';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+
+
 
 function Footer() {
-  return (
-    <footer>
-        <div className='ftr-with-menu'>
-        <div className="kss-footer-branding">
-            <div className="brnd-img">
-            <img src={Logo} alt="" />
-            <figcaption><b>Krishak Seva Sansthan</b></figcaption>
+    return (
+        <footer>
+            <div className='ftr-with-menu'>
+                <div className="kss-footer-branding">
+                    <div className="brnd-img">
+                        <img src={Logo} alt="" />
+                        <figcaption><b>Krishak Seva Sansthan</b></figcaption>
+                    </div>
+                    <div className="kss-social-media">
+                        <a href="#"><FacebookIcon /></a>
+                        <a href="#"><InstagramIcon /></a>
+                        <a href="#"><TwitterIcon /></a>
+                        <a href="#"><WhatsAppIcon /></a>
+                    </div>
+                </div>
+                <div className="kss-footer-menu">
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/blogs'>Blog</Link></li>
+                        <li><Link to='/about'>About Us</Link></li>
+                        <li><Link to='/contact'>Contact Us</Link></li>
+                        
+                    </ul>
+                </div>
+                <div className="kss-copyrights">
+                    <p><i>Copyright &copy; KRISHAK SEVA SANSTHAN.<br />
+                        All Rights are reserved.
+                    </i></p>
+                    <p><a href="#">Terms & Conditions</a></p>
+                </div>
             </div>
-            <div className="kss-social-media">
-                <a href="#">Home</a>
-                <a href="#">Instagram</a>
-                <a href="#">Tweitter</a>
-                <a href="#">Whatsapp</a>
-            </div>
-        </div>
-        <div className="kss-footer-menu">
-            <ul>
-                <li>Home</li>
-                <li>Blogs</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>
-        <div className="kss-copyrights">
-            <p><i>Copyright &copy; KRISHAK SEVA SANSTHAN.<br />
-                  All Rights are reserved.
-            </i></p>
-            <p><a href="#">Terms & Conditions</a></p>
-        </div>
-        </div>
 
-        
-    </footer>
-  )
+
+        </footer>
+    )
 }
 
 export default Footer
--- frontend/src/components/NavTools.jsx
+++ frontend/src/components/NavTools.jsx
@@ -6,6 +6,11 @@ import '../assets/css/Navbar.css'
 
 import TextIncreaseIcon from '@mui/icons-material/TextIncrease';
 import TextDecreaseIcon from '@mui/icons-material/TextDecrease';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 
 export default function Navbar() {
   return (
@@ -13,15 +18,19 @@ export default function Navbar() {
     <div className='kss-tool-wrapper'>
       <div className="kss-tool-container">
       <div>
-      <Button style={{marginRight:'10px'}} variant='outlined' color='inherit'><TextDecreaseIcon /></Button>
-      <Button variant='contained' color='inherit'><TextIncreaseIcon /></Button>
+        <a href="#"><FacebookIcon /></a>
+        <a href="#"><InstagramIcon /></a>
+        <a href="#"><TwitterIcon /></a>
+        <a href="#"><WhatsAppIcon /></a>
       </div>
       <div className='theme-col-container'>
-      <Button variant='contained' style={{backgroundColor:'inherit'}}></Button>
-      <Button variant='contained' style={{backgroundColor:'#008a1e'}}></Button>
-      <Button variant='contained' style={{backgroundColor:'#a20149'}}></Button>
-      <Button variant='contained' style={{backgroundColor:'#009dff'}}></Button>
-      <Button variant='contained' style={{backgroundColor:'#ff9100'}}></Button>
+      <Button style={{marginRight:'10px'}} variant='outlined' color='inherit'><TextDecreaseIcon /></Button>
+      <Button  variant='contained' color='inherit'><TextIncreaseIcon /></Button>
+      <Button className='kss-btn' variant='contained' style={{backgroundColor:'inherit'}}></Button>
+      <Button className='kss-btn' variant='contained' style={{backgroundColor:'#008a1e'}}></Button>
+      <Button className='kss-btn' variant='contained' style={{backgroundColor:'#a20149'}}></Button>
+      <Button className='kss-btn' variant='contained' style={{backgroundColor:'#009dff'}}></Button>
+      <Button className='kss-btn' variant='contained' style={{backgroundColor:'#ff9100'}}></Button>
     </div>
       </div>
     </div>
