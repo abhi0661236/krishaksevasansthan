d83a573739a55bab1d973cb8e94be9f79c335eab
abhi0661236 <ap0661236@gmail.com>
2022-06-19 12:15:14 +0530

    added our mission section

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/css/Footer.css';
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,6 +4,7 @@ import car1 from '../assets/media/carousel1.jpg';
 import car2 from '../assets/media/carousel2.jpg';
 import car3 from '../assets/media/carousel3.jpg';
 import avatar from '../assets/media/avatar.png';
+import Vermicompost from '../assets/media/vermicompost.jpeg';
 
 export default function Home() {
   return (
@@ -38,25 +39,25 @@ export default function Home() {
         </div>
         <div className="kss-lefthero col-md-4">
           <div className="kss-herocard">
-            <img src={avatar} alt=''/>
+            <img src={avatar} alt='' />
             <div>
 
-            <h4>Name</h4>
-            <p>Repellendus, aliquam!</p>
+              <h4>Name</h4>
+              <p>Repellendus, aliquam!</p>
             </div>
           </div>
           <div className="kss-herocard">
-            <img src={avatar} alt=''/>
+            <img src={avatar} alt='' />
             <div>
-            <h4>Name</h4>
-            <p>Repellendus, aliquam!</p>
+              <h4>Name</h4>
+              <p>Repellendus, aliquam!</p>
             </div>
           </div>
           <div className="kss-herocard">
-            <img src={avatar} alt=''/>
+            <img src={avatar} alt='' />
             <div>
-            <h4>Name</h4>
-            <p>Lorem ipsum aliquam!</p>
+              <h4>Name</h4>
+              <p>Lorem ipsum aliquam!</p>
             </div>
           </div>
         </div>
@@ -64,18 +65,27 @@ export default function Home() {
 
 
       <section className='kss-about'>
-      <div className="kss-sec-title">
-        <h4>Who We Are ?</h4>
-      </div>
-      <div className="kss-about-info">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia suscipit eius esse. Sunt provident cum earum, officia beatae ut corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. A, dolore assumenda eos consectetur iusto et neque ipsa eius nostrum, odio non rerum dicta consequatur ut explicabo fugit, similique culpa iste. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium voluptate rerum libero recusandae, nisi sunt qui maiores eaque atque sequi velit ratione fugiat eius. Molestias quae suscipit deleniti unde ipsum.         
-        </p>
-      </div>
-      <div className='kss-lower-about'>
+        <div className="kss-sec-title">
+          <h4>Who We Are ?</h4>
+        </div>
+        <div className="kss-about-info">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia suscipit eius esse. Sunt provident cum earum, officia beatae ut corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. A, dolore assumenda eos consectetur iusto et neque ipsa eius nostrum, odio non rerum dicta consequatur ut explicabo fugit, similique culpa iste. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium voluptate rerum libero recusandae, nisi sunt qui maiores eaque atque sequi velit ratione fugiat eius. Molestias quae suscipit deleniti unde ipsum.
+          </p>
+        </div>
+      </section>
+      <section className='kss-mission'>
+        <div className="kss-sec-title">
+          <h4>Our Mission</h4>
+        </div>
+        <div className="kss-mission-card1">
+          <h5>Vermi Composting</h5>
+          <p style={{color: '#5c5c5c'}}>Vermicompost (vermi-compost) is the product of the decomposition process using various species of worms, usually red wigglers, white worms, and other earthworms, to create a mixture of decomposing vegetable or food waste, bedding materials, and vermicast. This process is called vermicomposting.
+          </p>
+        </div>
+        <div className="kss-mission-card2">
+          <img src={Vermicompost} alt="" />
+        </div>
 
-      </div>
-      
-      
       </section>
 
     </main>
